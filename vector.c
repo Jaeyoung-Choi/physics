d95cde11c56e5962d116c2c07b277c57f0864f85
Jaeyoung Choi <40884210+Jaeyoung-Choi@users.noreply.github.com>
2019-06-05 01:42:36 +0900

    Add files via upload

--- vector.c
+++ vector.c
@@ -9,18 +9,43 @@ typedef struct Vector
     double z;
 } vector;
 
-double size (vector* v)
+double vector_get_size (vector* v)
 {
-    return sqrt(v.x * v.x + v.y * v.y + v.z *v.z);
+    return sqrt(v -> x * v -> x + v -> y * v -> y + v -> z *v -> z);
 }
 
-vector* sum (vector* v1, vector* v2)
+vector* vector_sum (vector* v1, vector* v2)
 {
     vector* res = (vector*)malloc(sizeof(vector));
-    res -> x = v1.x + v2.x;
-    res -> y = v1.y + v2.y;
-    res -> z = v1.z + v2.z;
+    res -> x = v1 -> x + v2 -> x;
+    res -> y = v1 -> y + v2 -> y;
+    res -> z = v1 -> z + v2 -> z;
     return res;
 }
 
-double inner_product (vector v1, vector v2)
+vector* vector_sub (vector* v, double s)
+{
+    vector* res = (vector*)malloc(sizeof(vector));
+    res -> x = v -> x * s;
+    res -> y = v -> y * s;
+    res -> z = v -> z * s;
+    return res;
+}
+
+vector* vector_mul (vector* v1, vector* v2)
+{
+    vector* res = (vector*)malloc(sizeof(vector));
+    res -> x = v1 -> x - v2 -> x;
+    res -> y = v1 -> y - v2 -> y;
+    res -> z = v1 -> z - v2 -> z;
+    return res;
+}
+
+double vector_inner_product (vector* v1, vector* v2)
+{
+    return v1 -> x * v2 -> x + v1 -> y * v2 -> y + v1  -> z * v2 -> z;
+}
+
+vector* vector_new_vector (double x, double y, double z)
+{
+    vector* 
