6c0efe3c06ad20251954259ea7761e9899a0664c
Jaeyoung Choi <40884210+Jaeyoung-Choi@users.noreply.github.com>
2019-12-04 14:44:30 +0900

    Update 3p.cpp

--- source/3p.cpp
+++ source/3p.cpp
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	Particle p1 = Particle(Vector(0, 1), Vector(1, 0, 0), 100000), p2 = Particle(Vector(0, 0, 1), Vector(0, 1, 0), 100000), p3 = Particle(Vector(1), Vector(0, 0, 1), 100000);
+	Particle p1 = Particle(Vector(0, 1), Vector(1, 0, 0), 100000), p2 = Particle(Vector(0, 0, 1), Vector(0, 1), 100000), p3 = Particle(Vector(1), Vector(0, 0, 1), 100000);
 	printf("P1: %.10Lf, %.10Lf, %.10Lf\n", p1.position.x, p1.position.y, p1.position.z);
 	printf("P2: %.10Lf, %.10Lf, %.10Lf\n", p2.position.x, p2.position.y, p2.position.z);
 	printf("P3: %.10Lf, %.10Lf, %.10Lf\n\n", p3.position.x, p3.position.y, p3.position.z);
